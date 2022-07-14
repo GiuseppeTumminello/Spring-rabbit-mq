b6e3a6e05eff6781ad6e0d579781180f3272bc69
GiuseppeTumminello <Giuseppe.Tumminello@acoustic.co>
2022-07-14 16:27:59 +0200

    deleted mapping added

--- RabbitMq-receiver/src/main/java/com/example/rabbitmqreceiver/controller/ReceiverController.java
+++ RabbitMq-receiver/src/main/java/com/example/rabbitmqreceiver/controller/ReceiverController.java
@@ -3,18 +3,15 @@ package com.example.rabbitmqreceiver.controller;
 import com.example.rabbitmqreceiver.entity.UserData;
 import com.example.rabbitmqreceiver.repository.UserRepository;
 import com.example.rabbitmqreceiver.service.SalaryCalculatorService;
+import io.netty.handler.codec.http.HttpResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -47,6 +44,17 @@ public class ReceiverController implements RabbitListenerConfigurer {
     public ResponseEntity<Optional<UserData>> getUserById(@PathVariable int  userId){
         var user = this.userRepository.findById(userId);
         return ResponseEntity.ok().header(HttpHeaders.ALLOW).body(user);
+    }
+
+    @DeleteMapping("/delete-user/{userId}")
+    public  ResponseEntity<String> deleteUserById(@PathVariable int userId){
+        var user = this.userRepository.findById(userId);
+        if (user.isPresent()) {
+            this.userRepository.deleteById(userId);
+            return ResponseEntity.ok().header(HttpHeaders.ALLOW).body("User with id: " + user.get().getId() + ", name: " + user.get().getName() + " deleted successfully" );
+        }
+            return ResponseEntity.badRequest().header(HttpHeaders.WARNING).body("User with id: " + userId + "is not present in the database");
+
     }
 
 
